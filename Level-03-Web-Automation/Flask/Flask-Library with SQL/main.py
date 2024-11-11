bc3aaaff4847e864840c403024f36ed68cb577db
Sva-Dagger <sivam4266@gmail.com>
2024-11-11 11:07:40 +0530

    update

--- Level-03-Web-Automation/Flask/Flask-Library with SQL/main.py
+++ Level-03-Web-Automation/Flask/Flask-Library with SQL/main.py
@@ -1,33 +1,81 @@
-from flask import Flask, render_template, request, redirect, url_for
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+from flask import Flask, render_template, redirect, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
 
 app = Flask(__name__)
 
-all_books = []
+## CREATE DATABASE
+class Base(DeclarativeBase):
+    pass
 
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///new-books-collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create the extension
+db = SQLAlchemy(model_class=Base)
+# Initialize the app with the extension
+db.init_app(app)
+
+
+## CREATE TABLE
+class Book(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
+
+# CREATE RECORD
+
+all_books = []
 
 @app.route('/')
 def home():
-    pass
+    all_books = Book.query.all()  # Retrieve all books from the database
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        book_title = request.form.get("title")
+        book_author = request.form.get("author")
+        book_rating = float(request.form.get("rating"))
+
+        # Check if a book with the same title already exists
+        existing_book = Book.query.filter_by(title=book_title).first()
+        if existing_book:
+            # If the book already exists, return an error message or redirect
+            return "Error: A book with this title already exists.", 400
+
+        # If the book does not exist, proceed with adding it
+        new_book = Book(title=book_title, author=book_author, rating=book_rating)
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for('home'))
+
+    return render_template("add.html")
+
+
+@app.route("/delete")
+def delete():
+    book_id = request.args.get('id')
+    # DELETE A RECORD BY ID
+    book_to_delete = Book.query.get(book_id)
+    if book_to_delete:
+        db.session.delete(book_to_delete)
+        db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
---
+++ Level-03-Web-Automation/Flask/Movie_Rating/main.py
@@ -0,0 +1,113 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6m'
+Bootstrap5(app)
+
+# CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Top-Movies-Collection.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Create the extension
+db = SQLAlchemy(model_class=Base)
+# Initialize the app with the extension
+db.init_app(app)
+
+# CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(String(250), nullable=False)
+    description: Mapped[str] = mapped_column(String(250), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=False)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=False)
+    review: Mapped[str] = mapped_column(String(250), nullable=False)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+    # Optional: this will allow each book object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Book {self.title}>'
+
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
+
+# CREATE RECORD
+
+all_Movies = []
+
+
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.5")
+    review = StringField("Your Review")
+    submit = SubmitField("Done")
+
+@app.route("/")
+def home():
+    all_Movies = Movie.query.all()
+    return render_template("index.html", movies=all_Movies)
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        Movie_title = request.form.get("title")
+        Movie_year = request.form.get("year")
+        Movie_rating = float(request.form.get("rating"))
+        Movie_description = request.form.get("description")
+        Movie_ranking = request.form.get("ranking")
+        Movie_review = request.form.get("review")
+        Movie_img = request.form.get("img_url")
+        # Check if a book with the same title already exists
+        existing_book = Movie.query.filter_by(title=Movie_title).first()
+        if existing_book:
+            # If the book already exists, return an error message or redirect
+            return "Error: A book with this title already exists.", 400
+
+        # If the book does not exist, proceed with adding it
+        new_Movie = Movie(title=Movie_title, year=Movie_year, description=Movie_description, ranking=Movie_ranking,
+                         rating=Movie_rating, review=Movie_review, img_url=Movie_img)
+        db.session.add(new_Movie)
+        db.session.commit()
+        return redirect(url_for('home'))
+
+# Adding the Update functionality
+@app.route("/edit", methods=["GET", "POST"])
+def rate_movie():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie = db.get_or_404(Movie, movie_id)
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        movie.review = form.review.data
+        db.session.commit()
+        return redirect(url_for('home'))
+    return render_template("edit.html", movie=movie, form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
